f464c61319bc91946b7932d308e2548005526704
hoangnm2k1 <hoangnm2k1@gmail.com>
2024-11-30 22:24:35 +0700

    create Monster-Rolodex app

---
+++ src/components/search-box/search-box.component.jsx
@@ -0,0 +1,22 @@
+import { ChangeEvent, ChangeEventHandler } from "react";
+import "./search-box.styles.css";
+
+// interface ISearchBoxProps {
+//   className: string;
+//   placeholder?: string;
+//   // onChangeHandler: ChangeEventHandler<HTMLInputElement>;
+//   onChangeHandler: (event: ChangeEvent<HTMLInputElement>) => void;
+// }
+
+const SearchBox = ({ className, placeholder, onChangeHandler }) => {
+  return (
+    <input
+      className={`search-box ${className}`}
+      type="search"
+      placeholder={placeholder}
+      onChange={onChangeHandler}
+    />
+  );
+};
+
+export default SearchBox;
